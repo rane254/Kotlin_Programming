26abfd33dee829e0aaf0ab20f58b93b5e1b1b6ac
rane254 <mr.appmaker.dev@gmail.com>
2025-05-01 18:21:05 +0530

    Class & Objects, Encapsulation

--- src/oops/practice/Main.kt
+++ src/oops/practice/Main.kt
@@ -1,6 +1,11 @@
 package oops.practice
+/*
+* 1. Classes and Objects
+* Classes are blueprints that define properties and behaviors.
+* Objects are instances of classes.
+*/
 
-// Class definition -> With header defined primary constructor
+// Class definition -> With header-defined primary constructor
 class Person(val name: String, var age: Int) {
 
     // The `init` block is the proper way to execute initialization
@@ -20,9 +25,59 @@ class Person(val name: String, var age: Int) {
     }
 }
 
+/*
+* 2. Encapsulation.
+* Encapsulation is about hiding internal states and
+* requiring all interaction to be performed through an object's methods.
+*/
+
+class BankAccount() {
+    private var balance: Double = 0.0
+
+    init {
+        print("Enter your current balance: ")
+        balance = readln().toDouble()
+    }
+    fun showBalance() {
+        // Public methods to interact with private data
+        println("-------------------------------")
+        println("Your balance: ${this.balance}")
+        println("-------------------------------")
+    }
+
+    fun creditBalance(amount: Double) {
+        // Public methods to interact with private data
+        if (amount > 0) {
+            balance += amount
+            println("Amount credited: $amount")
+            showBalance()
+        } else {
+            println("Entered amount should be greater than 0")
+        }
+    }
+
+    fun debitBalance(amount: Double) {
+        // Public methods to interact with private data
+        if (amount > 0 && amount <= this.balance) {
+            balance -= amount
+            println("Amount debited: $amount")
+            showBalance()
+        } else {
+            println("Entered amount should be greater than 0 & less than balance")
+            }
+    }
+}
+
+
+
 // main entry point of execution
 fun main() {
-    // Creating objects
-    val p1 = Person(name = "Brijesh", age = 25)
-    p1.canVote() // calling the object's method
+//    // Creating objects
+//    val p1 = Person(name = "Brijesh", age = 25)
+//    p1.canVote() // calling the object's method
+
+    val obj = BankAccount()
+    obj.showBalance()
+    obj.creditBalance(15000.0)
+    obj.debitBalance(10000.0)
 }
